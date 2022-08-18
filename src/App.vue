2b3855e9e10d8170ec3f0f3bad20b2fca724229b
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2022-08-18 19:24:13 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <Navbar/>
+    <h1>Hello</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/UI/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  box-sizing: border-box;
 }
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+h1,
+h2,
+h3,
+p {
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.container {
+  max-width: 1440px;
+  margin: auto;
+}
+
 </style>
---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <a href="">
+        <img src="../../image/logo.svg" />
+      </a>
+
+      <div class="header__desktop">
+        <nav>
+          <ul class="header__nav">
+            <li class="header__item">
+              <a class="header__links" href="">ALL PRODUCTS</a>
+            </li>
+            <li class="header__item">
+              <a class="header__links" href="">ABOUT SEEDRA</a>
+            </li>
+            <li class="header__item">
+              <a class="header__links" href="">OUR BLOG</a>
+            </li>
+            <li class="header__item">
+              <a class="header__links" href="">CONTACTS</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="header__media">
+          <a class="header__media-links">
+            <img src="../../image/icons/instagram.svg" alt="link to instagram" />
+          </a>
+          <a class="header__media-links">
+            <img src="../../image/icons/facebook.svg" alt="link to facebook" />
+          </a>
+        </div>
+
+        <form class="header__form">
+          <img class="header__form-img" src="../../image/input/search.svg" />
+          <input class="header__input" type="text" placeholder="Search" />
+        </form>
+
+        <div class="">    
+          <a class="header__actions-links">
+            <img class="" src="../../image/icons/max-heart.svg"/>
+          </a>
+          <a class="header__actions-links">
+            <img class="" src="../../image/icons/max-basket.svg"/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+}
+
+<script>
+export default {
+  name: 'navbar-header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/_variables.scss";
+
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &__desktop {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__nav {
+    display: flex;
+  }
+
+  &__item {
+    &:not(:last-child) {
+      border-right: 1px solid #efefef;
+      margin-right: 16px;
+    }
+  }
+
+  &__links {
+    color: $color-text-gray;
+    padding-right: 16px;
+  }
+
+  &__media-links:not(:last-child) {
+    margin-right: 12px;
+  }
+
+  &__media {
+    display: flex;
+    margin-right: 15px;
+  }
+
+  &__form {
+    width: 285px;
+    border: 1px solid #efefef;
+    border-radius: 71px;
+    padding: 0 24px;
+    display: flex;
+    margin-right: 15px;
+  }
+
+  &__form-img {
+    margin-right: 16px;
+  }
+
+  &__input {
+    border: none;
+    font-size: 14px;
+    line-height: 178.19%;
+    height: 48px;
+    // padding: 0 20px;
+    outline: none;
+  }
+
+  &__actions-links {
+    &:not(:last-child) {
+      margin-right: 28px;
+    }
+  }
+}
+</style>
